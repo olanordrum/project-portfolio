0a809be0fe9d7536885f4464e88279377fbd0438
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-08-06 00:30:19 +0200

    Add simple header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/footer";
+import Header from "../components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Header />
         <main className="mx-auto w-full max-w-4xl px-4 py-8">{children}</main>
         <Footer />
       </body>
---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="border-b">
+      <div className="max-w-6xl mx-auto px-6 flex align-center justify-between ">
+        <p>Ola Nordrum Isachsen</p>
+        <nav>Projects</nav>
+      </div>
+    </header>
+  );
+}
