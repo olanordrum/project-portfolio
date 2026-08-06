3096e8079ca1c7eec313f73615416173c87ff98e
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-08-06 23:08:41 +0200

    Update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Space_Grotesk, Manrope } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/footer";
 import Header from "../components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manrope = Manrope({
+  variable: "--font-body",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-display-font",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -27,9 +28,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${manrope.variable} ${spaceGrotesk.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col font-sans">
         <Header />
         <main className="mx-auto w-full max-w-4xl px-4 py-8">{children}</main>
         <Footer />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,10 @@ export default function Header() {
   return (
     <header className="border-b">
       <div className=" mx-auto flex items-center justify-between px-4 py-4 md:px-8 lg:px-12  ">
-        <p className="text-lg md:text-xl lg:text-2xl ">Ola Nordrum Isachsen</p>
-        <nav className="text-lg">Projects</nav>
+        <p className="font-display font-bold text-lg md:text-xl lg:text-3xl ">
+          OLA NORDRUM ISACHSEN
+        </p>
+        <nav className="text-xl">Projects</nav>
       </div>
     </header>
   );
