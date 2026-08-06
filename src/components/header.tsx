02b4d002847bd18f29fe0be4f35455d01a18f220
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-08-06 21:13:37 +0200

    Fix styling for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
     <header className="border-b">
-      <div className="max-w-6xl mx-auto px-6 flex align-center justify-between ">
-        <p>Ola Nordrum Isachsen</p>
-        <nav>Projects</nav>
+      <div className=" mx-auto flex items-center justify-between px-4 py-4 md:px-8 lg:px-12  ">
+        <p className="text-lg md:text-xl lg:text-2xl ">Ola Nordrum Isachsen</p>
+        <nav className="text-lg">Projects</nav>
       </div>
     </header>
   );
