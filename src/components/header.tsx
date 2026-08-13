12ca6901507410700a3d21ffd7a649dc13aec387
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-08-13 23:07:55 +0200

    Add navigation to text in header. Remove "Home" from footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,21 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full bg-white">
       <div className=" mx-auto flex items-center justify-between px-4 py-4 md:px-8 lg:px-12  ">
-        <p className="font-display font-semibold text-lg md:text-xl lg:text-3xl duration-300 hover:tracking-wide cursor-pointer ">
+        <Link
+          href="/"
+          className="font-display font-semibold text-lg md:text-xl lg:text-3xl duration-300 hover:tracking-wide cursor-pointer "
+        >
           OLA NORDRUM ISACHSEN
-        </p>
-        <a
-          href="#projects"
+        </Link>
+        <Link
+          href="/#projects"
           className="text-xl duration-300 hover:tracking-wide "
         >
           Projects
-        </a>
+        </Link>
       </div>
     </header>
   );
