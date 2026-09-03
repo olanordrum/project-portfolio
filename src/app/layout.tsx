e275780da4830bb26d80edd27196d6f1b6d4fb59
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-09-03 22:46:43 +0200

    Fix layout for home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col font-sans">
         <Header />
-        <main className="mx-auto w-full max-w-4xl px-4 py-8 flex flex-1">
-          {children}
-        </main>
+        <main className="w-full flex flex-1">{children}</main>
         <Footer />
       </body>
     </html>
