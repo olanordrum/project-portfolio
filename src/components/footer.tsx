f3aa7052d691ffb1686ac054ad2cc526bb1a243d
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-09-03 22:46:13 +0200

    Change footer background color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="border-t mt-auto py-4 text-sm text-center text-gray-500">
+    <footer className="bg-white mt-auto py-4 text-sm text-center text-gray-500">
       <nav className="flex justify-center gap-6">
         <Link href="/admin">Admin</Link>
       </nav>
