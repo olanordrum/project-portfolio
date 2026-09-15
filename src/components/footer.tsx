bd61ba43a237c8928c902992772496abfdd84d14
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-09-16 00:13:14 +0200

    Add Structure for multiple links in footer - including print

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,26 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-white mt-auto py-4 text-sm text-center text-gray-500">
-      <nav className="flex justify-center gap-6">
-        <Link href="/admin">Admin</Link>
-      </nav>
+    <footer className="bg-[f4f5ee] border-t border-white border-t-3 mt-auto py-4 text-sm text-center text-gray-500">
+      <div className="flex items-center justify-center gap-15">
+        <img
+          src={"../../old_dude.png"}
+          alt="backflip animation"
+          className="h-6 md:h-9 lg:h-30 w-auto object-contain"
+        />
+        <nav className="flex flex-col justify-center items-start gap-2 tracking-wider">
+          <p className="text-black font-bold">NAVIGATION</p>
+          <Link className="hover:text-[#3E64CE] tracking-wide" href="/admin">
+            Admin
+          </Link>
+        </nav>
+        <nav className="flex flex-col justify-center items-start gap-2 tracking-wider">
+          <p className="text-black font-bold">SOCIAL</p>
+          <Link className="hover:text-[#3E64CE] tracking-wide" href="/admin">
+            Admin
+          </Link>
+        </nav>
+      </div>
     </footer>
   );
 }
