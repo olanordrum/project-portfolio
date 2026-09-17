5b0f2d7fc9c34832a7f30d26fe7cbf347bceea45
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-09-17 20:04:38 +0200

    Add liks to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,25 +2,40 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-[f4f5ee] border-t border-white border-t-3 mt-auto py-4 text-sm text-center text-gray-500">
+    <footer className=" mt-auto py-10 text-sm text-center ">
       <div className="flex items-center justify-center gap-15">
-        <img
-          src={"../../old_dude.png"}
-          alt="backflip animation"
-          className="h-6 md:h-9 lg:h-30 w-auto object-contain"
-        />
-        <nav className="flex flex-col justify-center items-start gap-2 tracking-wider">
-          <p className="text-black font-bold">NAVIGATION</p>
-          <Link className="hover:text-[#3E64CE] tracking-wide" href="/admin">
-            Admin
-          </Link>
-        </nav>
-        <nav className="flex flex-col justify-center items-start gap-2 tracking-wider">
-          <p className="text-black font-bold">SOCIAL</p>
-          <Link className="hover:text-[#3E64CE] tracking-wide" href="/admin">
-            Admin
-          </Link>
-        </nav>
+        <div className="flex items-start gap-7 lg:gap-10">
+          <nav className="flex flex-col justify-center items-start gap-2">
+            <p className="tracking-widest font-bold">NAVIGATION</p>
+            <Link
+              className="hover:tracking-widest transition-all duration-300"
+              href="/#projects"
+            >
+              Projects
+            </Link>
+            <Link
+              className="hover:tracking-widest transition-all duration-300"
+              href="/admin"
+            >
+              Admin
+            </Link>
+          </nav>
+          <nav className="flex flex-col justify-center items-start gap-2">
+            <p className="tracking-widest not-last:font-bold">CONTACT</p>
+            <Link
+              className="hover:tracking-widest transition-all duration-300"
+              href="https://www.linkedin.com/in/olanordrum"
+            >
+              linkedin
+            </Link>
+            <Link
+              className="hover:tracking-widest transition-all duration-300"
+              href="https://github.com/olanordrum"
+            >
+              Github
+            </Link>
+          </nav>
+        </div>
       </div>
     </footer>
   );
